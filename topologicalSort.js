4f4233b0ad695202887e444c9b01af5e01cdc23d
vladilin <ilin96inbox@gmail.com>
2020-04-27 09:13:12 +0900

    topological sort

---
+++ topologicalSort.js
@@ -0,0 +1,79 @@
+// O(j + d) time | O(j + d) space
+function topologicalSort(jobs, deps) {
+  const jobGraph = createJobGraph(jobs, deps); //?
+  return getOrderedJobs(jobGraph); //?
+}
+
+function createJobGraph(jobs, deps) {
+  const graph = new JobGraph(jobs);
+  for (const [job, dep] of deps) {
+    graph.addDep(job, dep);
+  }
+  return graph;
+}
+
+function getOrderedJobs(graph) {
+  const orderedJobs = [];
+  const nodesWithNoPrereqs = graph.nodes.filter((node) => !node.numOfPrereqs); //?
+  while (nodesWithNoPrereqs.length) {
+    const node = nodesWithNoPrereqs.pop();
+    orderedJobs.push(node.job); //?
+    removeDeps(node, nodesWithNoPrereqs);
+  }
+  const graphHasEdges = graph.nodes.some((node) => node.numOfPrereqs); //?
+  return graphHasEdges ? [] : orderedJobs; //?
+}
+
+function removeDeps(node, nodesWithNoPrereqs) {
+  while (node.deps.length) {
+    const dep = node.deps.pop();
+    dep.numOfPrereqs--;
+    if (!dep.numOfPrereqs) nodesWithNoPrereqs.push(dep);
+  }
+}
+
+class JobGraph {
+  constructor(jobs) {
+    this.nodes = [];
+    this.graph = {};
+    for (const job of jobs) {
+      this.addNode(job);
+    }
+  }
+
+  addDep(job, dep) {
+    const jobNode = this.getNode(job);
+    const depNode = this.getNode(dep);
+    jobNode.deps.push(depNode);
+    depNode.numOfPrereqs++;
+  }
+
+  addNode(job) {
+    this.graph[job] = new JobNode(job);
+    this.nodes.push(this.graph[job]);
+  }
+
+  getNode(job) {
+    if (!(job in this.graph)) this.addNode(job);
+    return this.graph[job];
+  }
+}
+
+class JobNode {
+  constructor(job) {
+    this.job = job;
+    this.deps = [];
+    this.numOfPrereqs = 0;
+  }
+}
+
+topologicalSort(
+  [1, 2, 3, 4],
+  [
+    [1, 2],
+    [1, 3],
+    [3, 2],
+    [4, 2],
+    [4, 3],
+  ]
+);
