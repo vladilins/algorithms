16820c77d1b7b0a27a7f0bb4877b93bde1a5c11c
vladilin <ilin96inbox@gmail.com>
2020-05-22 20:41:12 +0900

    min height BST

---
+++ minHeightBst.js
@@ -0,0 +1,36 @@
+function minHeightBst(array) {
+  return constructMinHight(array, 0, array.length - 1);
+}
+
+function constructMinHight(array, startIdx, endIdx) {
+  if (startIdx > endIdx) return null;
+  const midIdx = Math.floor((startIdx + endIdx) / 2);
+  const bst = new BST(array[midIdx]);
+  bst.left = constructMinHight(array, startIdx, midIdx - 1);
+  bst.right = constructMinHight(array, midIdx + 1, endIdx);
+  return bst;
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+}
