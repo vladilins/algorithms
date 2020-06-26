faa69a209c8af9cca4dbb5b2424d44b401169ce9
vladilin <ilin96inbox@gmail.com>
2020-06-26 09:00:44 +0900

    find disappeared numbers

---
+++ findDisappearedNumbers(448).js
@@ -0,0 +1,19 @@
+function findDisappearedNumbers(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let j = Math.abs(nums[i]) - 1; //?
+    nums[j] = Math.abs(nums[j]) * -1; //?
+  }
+  console.log(nums);
+
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+
+  return result; //?
+}
+
+findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]);
