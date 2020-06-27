aa61cb40c38dcac37789a865ed265a7165df2cb4
vladilin <ilin96inbox@gmail.com>
2020-06-27 10:13:44 +0900

    merge lists

--- mergeLinkLists.js
+++ mergeLinkLists.js
@@ -18,3 +18,23 @@ function mergeLinkLists(headOne, headTwo) {
   if (p1 === null) p1Prev.next = p2;
   return headOne.value < headTwo.value ? headOne : headTwo;
 }
+
+function mergeTwoLists(l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
+
+var mergeTwoLists = function (l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2;
+  if (l1.val > l2.val) {
+    [l1, l2] = [l2, l1];
+  }
+  l1.next = mergeTwoLists(l1.next, l2);
+  return l1 || l2;
+};
