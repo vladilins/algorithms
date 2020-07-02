1b220d563d79df310392ee88b0a76370eda64a64
vladilin <ilin96inbox@gmail.com>
2020-07-03 07:56:46 +0900

    palindrome list

---
+++ palindromeList(234).js
@@ -0,0 +1,27 @@
+// O(n) time | O(1) space
+function palindromeList(head) {
+  let slow = head;
+  let fast = head;
+  // find middle
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  // reverse half
+  let node = null;
+  while (slow) {
+    const next = slow.next;
+    slow.next = node;
+    node = slow;
+    slow = next;
+  }
+
+  // compare
+  while (node) {
+    if (node.val !== head.val) return false;
+    node = node.next;
+    head = head.next;
+  }
+  return true;
+}
