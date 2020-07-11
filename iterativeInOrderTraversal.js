4a7cb803887015bc2ec9846a1ed4d16d04316c27
Vladislav Ilin <vlad@MacBook-Pro.local>
2020-07-12 07:19:31 +0900

    in order traversal

--- iterativeInOrderTraversal.js
+++ iterativeInOrderTraversal.js
@@ -36,3 +36,19 @@ iterativeInOrderTraversal({
   ],
   root: "1",
 });
+
+var inorderTraversal = function (root) {
+  const res = [];
+  const stack = [];
+  let curr = root;
+  while (curr || stack.length) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    curr = stack.pop();
+    res.push(curr.val);
+    curr = curr.right;
+  }
+  return res;
+};
