7bcfa5895024998b4511ac4a44903f89b0b41726
Vladislav Ilin <ilin96inbox@gmail.com>
2020-08-08 17:47:31 +0900

    decode string

---
+++ decodeString(394).js
@@ -0,0 +1,31 @@
+function decodeString(s) {
+  const stack = [];
+  for (let char of s) {
+    if (char !== "]") {
+      stack.push(char);
+      continue;
+    }
+    // get letters until [
+    let cur = stack.pop(); //?
+    let str = "";
+    while (cur !== "[") {
+      str = cur + str; //?
+      cur = stack.pop(); //?
+    }
+    // get multiplier
+    let num = "";
+    cur = stack.pop(); //?
+    while (Number.isInteger(+cur)) {
+      num = cur + num; //?
+      cur = stack.pop(); //?
+    }
+    // push current value
+    stack.push(cur);
+    // push str * num
+    stack.push(str.repeat(Number(num)));
+  }
+  return stack.join(""); //?
+}
+
+// decodeString("3[a]2[bc]");
+decodeString("3[a2[c]]");
