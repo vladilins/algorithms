1832bd32491aa2e22f9d513ca529c6ed2815d1d5
Vlad <ilin96inbox@gmail.com>
2020-09-03 09:42:13 +0900

    binary search

---
+++ binarySearch.js
@@ -0,0 +1,18 @@
+function binarySearch(array, target) {
+  return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(array, target, left, right) {
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    const potentialMatch = array[middle];
+    if (potentialMatch === target) {
+      return middle;
+    } else if (potentialMatch < target) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return -1;
+}
