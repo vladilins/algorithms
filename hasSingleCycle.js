81ee33a9569a8a4002b86fd6fdc8e441394ad0a3
Vlad <ilin96inbox@gmail.com>
2020-09-12 20:49:53 +0900

    bfs

--- hasSingleCycle.js
+++ hasSingleCycle.js
@@ -1,3 +1,4 @@
+// O(n) time | O(1) space
 function hasSingleCycle(array) {
   let elementsVisited = 0;
   let currentIdx = 0;
@@ -6,9 +7,12 @@ function hasSingleCycle(array) {
     elementsVisited++;
     currentIdx = getNextIdx(array, currentIdx);
   }
+  return currentIdx === 0;
 }
 function getNextIdx(array, i) {
   const jump = array[i];
   const nextIdx = (i + jump) % array.length;
   return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
 }
+
+hasSingleCycle(2, 3, 1, -4, -4, 2); //?
