8c30cdaf163d34cb714e35942e90e471d29e1250
Vlad <ilin96inbox@gmail.com>
2021-02-14 19:13:21 +0900

    sort

---
+++ python/mergeSort.py
@@ -0,0 +1,43 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              # The value from the left half has been used
+              arr[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k]=right[j]
+            j += 1
+            k += 1
+
+arr = [54,26,93,17,77,31,44,55,20]
+mergeSort(arr)
+print(arr)
