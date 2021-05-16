1c94da6e87572ac7802a5b03c980a8239389abb5
Vlad <ilin96inbox@gmail.com>
2021-05-16 20:24:19 +0900

    algo

--- python/mergeSort.py
+++ python/mergeSort.py
@@ -11,16 +11,16 @@ def mergeSort(arr):
         # Two iterators for traversing the two halves
         i = 0
         j = 0
-        
+
         # Iterator for the main list
         k = 0
-        
+
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
-              # The value from the left half has been used
-              arr[k] = left[i]
-              # Move the iterator forward
-              i += 1
+                # The value from the left half has been used
+                arr[k] = left[i]
+                # Move the iterator forward
+                i += 1
             else:
                 arr[k] = right[j]
                 j += 1
@@ -34,10 +34,11 @@ def mergeSort(arr):
             k += 1
 
         while j < len(right):
-            arr[k]=right[j]
+            arr[k] = right[j]
             j += 1
             k += 1
 
-arr = [54,26,93,17,77,31,44,55,20]
+
+arr = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 mergeSort(arr)
 print(arr)
